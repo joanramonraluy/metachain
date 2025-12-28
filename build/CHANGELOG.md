4bf5152abf40b3f6f368f0c21b4dd1b4a2a29b17
JoanRamon <joanramonraluy@gmail.com>
2025-12-28 10:11:16 +0100

    chore: clean repo, remove ignored files

--- build/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-##### [0.0.1] - 10 December 2025
-
-- Initial version
--- node_modules/@tanstack/router-devtools-core/dist/esm/index.js
+++
@@ -1,6 +0,0 @@
-import { T, v } from "../index-DX7UIGS3.js";
-export {
-  T as TanStackRouterDevtoolsCore,
-  v as TanStackRouterDevtoolsPanelCore
-};
-//# sourceMappingURL=index.js.map
